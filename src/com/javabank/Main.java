d7e36cb194e73d508b088f0fd6a841eafb668da4
Pedro Ferreira <pedrofmj@gmail.com>
2022-03-19 14:49:37 -0300

    Suporte a JSON

--- src/com/javabank/Main.java
+++ src/com/javabank/Main.java
@@ -1,10 +1,35 @@
 package com.javabank;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
+        // TODO
+    }
 
-        System.out.println("Hello, World!");
+    public static void main_json(String[] args) {
+
+        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("chave1", "valor1");
+        map.put("chave2", "valor2");
+        map.put("chave3", "valor3");
+
+        String str = gson.toJson(map);
+
+        System.out.println(str);
+
+    }
+
+    public static void main_hello(String[] args) {
+
+        System.out.println("hello");
 
     }
 }
